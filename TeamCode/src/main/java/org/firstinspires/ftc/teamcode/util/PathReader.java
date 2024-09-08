7f6d9357d92f41c0f5924ce8a43ef864d42ec6b3
Astro064 <thomas135969@brisbanegrammar.com>
2024-09-08 13:17:26 +1000

    Create PathReader.java

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/util/PathReader.java
@@ -0,0 +1,17 @@
+package org.firstinspires.ftc.teamcode.util;
+
+public class PathReader {
+
+
+
+
+
+
+    //Pass in path into file
+    //Read and assign x , y and direction into array list
+
+    // node 0 - x, y , dir
+    //node 1 - x1, y1, dir 1
+
+
+}
